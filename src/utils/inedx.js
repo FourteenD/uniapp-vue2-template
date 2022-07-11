dc452f010b89dd88425eafb7b413ac89708f9158
FourteenD <2899385645@qq.com>
2022-07-11 11:16:45 +0800

    export default {   showLoading,   hideLoading,   showModal,   showToast,   showMsg,   removeData,   setData,   getData,   parseUrl,   getAuthToken,   checkStr, };

---
+++ src/utils/inedx.js
@@ -0,0 +1,130 @@
+export const showLoading = (title = "加载中") => {
+  uni.showLoading({
+    title: title,
+    mask: true,
+  });
+};
+export const hideLoading = () => {
+  uni.hideLoading();
+};
+export const showModal = (content) => {
+  uni.showModal({
+    title: "提示",
+    content: content,
+    showCancel: false,
+    cancelText: "",
+    confirmText: "确定",
+  });
+};
+export const showToast = (title, callBack) => {
+  uni.showToast({
+    title: title,
+    mask: true,
+    complete: callBack,
+  });
+};
+export const showMsg = (title, callBack) => {
+  uni.showToast({
+    title: title,
+    icon: "none",
+    mask: true,
+    complete: callBack,
+  });
+};
+export const removeData = (key) => {
+  try {
+    uni.removeStorageSync(key);
+  } catch (err) {}
+};
+export const setData = (key, val) => {
+  uni.setStorageSync(key, val);
+};
+export const getData = function (key) {
+  try {
+    return uni.getStorageSync(key);
+  } catch (err) {
+    return null;
+  }
+};
+
+/**
+ * 获得认证 Token
+ *
+ * @return 认证 Token
+ */
+export const getAuthToken = () => {
+  return uni.getStorageSync("token");
+};
+
+/**
+ * 解析URL
+ *
+ * @param {String} 字符串
+ */
+export const parseUrl = (url) => {
+  var pattern = /(\w+)=(\w+)/gi;
+  var parames = {};
+  url.replace(pattern, function (a, b, c) {
+    parames[b] = c;
+  });
+  return parames;
+};
+
+/**
+ * 校验参数
+ *
+ * @param {String} 字符串
+ * @param {String}  数据的类型。例如说 mobile 手机号、tel 座机 TODO 芋艿：是否组件里解决
+ */
+export const checkStr = (str, type) => {
+  switch (type) {
+    case "mobile": //手机号码
+      return /^1[3|4|5|6|7|8|9][0-9]{9}$/.test(str);
+    case "tel": //座机
+      return /^(0\d{2,3}-\d{7,8})(-\d{1,4})?$/.test(str);
+    case "card": //身份证
+      return /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/.test(str);
+    case "mobileCode": //6位数字验证码
+      return /^[0-9]{6}$/.test(str);
+    case "pwd": //密码以字母开头，长度在6~18之间，只能包含字母、数字和下划线
+      return /^([a-zA-Z0-9_]){6,18}$/.test(str);
+    case "payPwd": //支付密码 6位纯数字
+      return /^[0-9]{6}$/.test(str);
+    case "postal": //邮政编码
+      return /[1-9]\d{5}(?!\d)/.test(str);
+    case "QQ": //QQ号
+      return /^[1-9][0-9]{4,9}$/.test(str);
+    case "email": //邮箱
+      return /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/.test(str);
+    case "money": //金额(小数点2位)
+      return /^\d*(?:\.\d{0,2})?$/.test(str);
+    case "URL": //网址
+      return /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?/.test(
+        str
+      );
+    case "IP": //IP
+      return /((?:(?:25[0-5]|2[0-4]\\d|[01]?\\d?\\d)\\.){3}(?:25[0-5]|2[0-4]\\d|[01]?\\d?\\d))/.test(
+        str
+      );
+    case "date": //日期时间
+      return (
+        /^(\d{4})\-(\d{2})\-(\d{2}) (\d{2})(?:\:\d{2}|:(\d{2}):(\d{2}))$/.test(
+          str
+        ) || /^(\d{4})\-(\d{2})\-(\d{2})$/.test(str)
+      );
+    case "number": //数字
+      return /^[0-9]$/.test(str);
+    case "english": //英文
+      return /^[a-zA-Z]+$/.test(str);
+    case "chinese": //中文
+      return /^[\\u4E00-\\u9FA5]+$/.test(str);
+    case "lower": //小写
+      return /^[a-z]+$/.test(str);
+    case "upper": //大写
+      return /^[A-Z]+$/.test(str);
+    case "HTML": //HTML标记
+      return /<("[^"]*"|'[^']*'|[^'">])*>/.test(str);
+    default:
+      return true;
+  }
+};
