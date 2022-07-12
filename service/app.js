e1d1ead0c3f7e734b71ac90fedc4233764b4f5ca
FourteenD <2899385645@qq.com>
2022-07-12 13:53:42 +0800

    new file:   service/request.http

--- service/app.js
+++ service/app.js
@@ -3,5 +3,13 @@ const Router = require("koa-router");
 const app = new Koa();
 const router = new Router();
 
+router.get("/", async (ctx, next) => {
+  var name = ctx.params.name;
+  ctx.response.body = `<h5>Hello, ${name}!</h5>`;
+});
+
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.listen(3000, () => {
+  console.log("服务运行中:http://127.0.0.1:3000");
+});
