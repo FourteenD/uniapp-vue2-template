ddc416321f8d25c062d329f235a9c2d4befce46a
FourteenD <2899385645@qq.com>
2022-07-12 16:07:06 +0800

    up

---
+++ mock/index.js
@@ -0,0 +1,23 @@
+const proxy = {
+  "/api/login/account": (req, res) => {
+    const { password, username } = req.body;
+    if (password === "888888" && username === "admin") {
+      return res.json({
+        status: "ok",
+        code: 0,
+        token: "sdfsdfsdfdsf",
+        data: {
+          id: 1,
+          username: "kenny",
+          sex: 6,
+        },
+      });
+    } else {
+      return res.status(403).json({
+        status: "error",
+        code: 403,
+      });
+    }
+  },
+};
+module.exports = proxy;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-// vue.config.js
+const path = require("path");
+const apiMocker = require("mocker-api");
+
 module.exports = {
+  devServer: {
+    before(app) {
+      apiMocker(app, path.resolve("./mock/index.js"), {
+        proxy: {
+          "/repos/(.*)": "https://api.github.com/",
+        },
+        changeHost: true,
+      });
+    },
+  },
   transpileDependencies: ["uview-ui"],
 };
