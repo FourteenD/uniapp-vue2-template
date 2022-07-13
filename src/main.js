558681dbbc2800833b4934cdd70025a50d74d343
FourteenD <2899385645@qq.com>
2022-07-13 11:39:09 +0800

    up

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import Vue from "vue";
 import App from "./App";
-import "@/plugins";
+
+import mixin from "./common/mixin";
+
+// vuex
+import store from "./store";
+Vue.prototype.$store = store;
 
 Vue.config.productionTip = true;
 
 App.mpType = "app";
+
+// 引入全局uView
+import uView from "uview-ui";
+Vue.use(uView);
+
+// #ifdef MP
+// 引入uView对小程序分享的mixin封装
+const mpShare = require("uview-ui/libs/mixin/mpShare.js");
+Vue.mixin(mpShare);
+// #endif
+
+Vue.mixin(mixin);
 
 const app = new Vue({
   ...App,
--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,6 @@ import Vuex from "vuex";
 import getters from "./getters";
 import { importModules } from "@/utils";
 Vue.use(Vuex);
-const modules = importModules("./modules", true, /\.js$/);
+const modules = importModules("./modules", /\.js$/);
 const store = new Vuex.Store({ modules, getters });
 export default store;
